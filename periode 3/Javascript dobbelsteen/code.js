4d054df3fb9469ffdc0aa2ac7526f48beed8416e
Niels284 <40193974@roctilburg.nl>
2023-03-08 22:14:42 +0100

    updated - added arrow functions and objects

--- periode 3/Javascript dobbelsteen/code.js
+++ periode 3/Javascript dobbelsteen/code.js
@@ -1,62 +1,71 @@
-// variables
-let averageDieValue = 0;
-let countedLogs = [];
-let valueLogs = [];
+const history = {
+  countedLogs: [],
+  valueLogs: [],
+  // calculate the average value and return the average
+  get average() {
+    return (
+      this.valueLogs.reduce((a, b) => a + b) / this.valueLogs.length
+    ).toFixed(0);
+  },
+  set logs(dieValue) {
+    this.valueLogs.push(dieValue);
+    console.log(`current value: ${dieValue}`);
+    // push the dieValue in the object based array 'countedLogs'
+    if (this.countedLogs.length == 0) {
+      this.countedLogs[this.countedLogs.length] = {
+        dobbelsteenWaarde: dieValue,
+        count: 1,
+      };
+    } else {
+      let alreadyAdded = false;
+      this.countedLogs.forEach((key) => {
+        if (key.dobbelsteenWaarde === dieValue) {
+          key.count++;
+          alreadyAdded = true;
+        }
+      });
+      if (alreadyAdded === false) {
+        this.countedLogs[this.countedLogs.length + 1] = {
+          dobbelsteenWaarde: dieValue,
+          count: 1,
+        };
+      }
+    }
+  },
+};
 
+// selections
+const sortButton = document.querySelector(".sortButton");
+
+// initialize the page
 function init() {
-  // initialize the page
   roll();
   loadCountedLogs();
   console.log("code.js is succesfully loaded");
 }
 
 function roll() {
-  // selections
-  const dobbelsteen = document.querySelector(".dobbelsteen");
-  const valueLogsList = document.querySelector(".valueLogsList");
-  const valueLogsList2 = document.querySelector(".valueLogsList2");
-  const averageValue = document.querySelector(".averageValue");
   // generated dobbelsteen value
-  const dobbelsteenValue = Math.floor(Math.random() * 6 + 1);
-  dobbelsteen.innerHTML = `<object data="img/svg/dobbelsteen_${dobbelsteenValue}.svg" width="200" height="200"></object>`;
+  const dieValue = Math.floor(Math.random() * 6 + 1);
+  history.logs = dieValue;
+  document.querySelector(
+    ".dobbelsteen"
+  ).innerHTML = `<object data="img/svg/dobbelsteen_${dieValue}.svg" width="200" height="200"></object>`;
   // logs the values in the valueLogs list and display it in the DOM
-  valueLogs.push(dobbelsteenValue);
-  valueLogsList2.innerHTML += `<li><p>${valueLogs.length} draw = </p><object data="img/svg/dobbelsteen_${dobbelsteenValue}.svg" width="50" height="50"></object></li>`;
-  console.log(`current value: ${dobbelsteenValue}`);
-  valueLogsList.innerHTML += `<li><object data="img/svg/dobbelsteen_${dobbelsteenValue}.svg" width="100" height="100"></object></li>`;
-  // push the dobbelsteenValue in the object based array 'countedLogs'
-  if (countedLogs.length == 0) {
-    countedLogs[countedLogs.length] = {
-      dobbelsteenWaarde: dobbelsteenValue,
-      count: 1,
-    };
-  } else {
-    let alreadyAdded = false;
-    countedLogs.forEach((key) => {
-      if (key.dobbelsteenWaarde === dobbelsteenValue) {
-        key.count++;
-        alreadyAdded = true;
-      }
-    });
-    if (alreadyAdded === false) {
-      countedLogs[countedLogs.length + 1] = {
-        dobbelsteenWaarde: dobbelsteenValue,
-        count: 1,
-      };
-    }
-  }
-  // calculate the average value and display it in the DOM
-  let calculatedValues = valueLogs.reduce(function (a, b) {
-    return a + b;
-  });
-  averageDieValue = (calculatedValues / valueLogs.length).toFixed(0);
-  averageValue.innerHTML = `The average die value is: ${averageDieValue}`;
+  document.querySelector(
+    ".valueLogsList"
+  ).innerHTML += `<li><object data="img/svg/dobbelsteen_${dieValue}.svg" width="100" height="100"></object></li>`;
+  document.querySelector(
+    ".valueLogsList2"
+  ).innerHTML += `<li><p>${history.valueLogs.length} draw = </p><object data="img/svg/dobbelsteen_${dieValue}.svg" width="50" height="50"></object></li>`;
+  document.querySelector(
+    ".averageValue"
+  ).innerText = `The average die value is: ${history.average}`;
   loadCountedLogs();
 }
 
 // change the data attribute 'data-order' of the .countedLogs
 function sortCountedLogs() {
-  const sortButton = document.querySelector(".sortButton");
   if (sortButton.dataset.order === "count") {
     sortButton.dataset.order = "dobbelsteenWaarde";
   } else if (sortButton.dataset.order === "dobbelsteenWaarde") {
@@ -67,12 +76,9 @@ function sortCountedLogs() {
 
 // load the countedLogs in the DOM
 function loadCountedLogs() {
-  const countedValues = document.querySelector(".countedValues");
-  const sortButton = document.querySelector(".sortButton");
-
   if (sortButton.dataset.order === "count") {
     // sort countedLogs DESC by count
-    countedLogs.sort(function (a, b) {
+    history.countedLogs.sort(function (a, b) {
       if (a.count > b.count) {
         return -1;
       } else if (a.count < b.count) {
@@ -84,7 +90,7 @@ function loadCountedLogs() {
     sortButton.innerHTML = "Order ASC by dobbelsteen values";
   } else if (sortButton.dataset.order === "dobbelsteenWaarde") {
     // sort countedLogs ASC by dobbelsteenWaarde
-    countedLogs.sort(function (a, b) {
+    history.countedLogs.sort(function (a, b) {
       if (a.dobbelsteenWaarde < b.dobbelsteenWaarde) {
         return -1;
       } else if (a.dobbelsteenWaarde > b.dobbelsteenWaarde) {
@@ -95,9 +101,12 @@ function loadCountedLogs() {
     });
     sortButton.innerHTML = "Order DESC by counted dobbelsteen values";
   }
+  console.log(history.countedLogs);
   // display it in the DOM
-  countedValues.innerHTML = "";
-  countedLogs.forEach((key) => {
-    countedValues.innerHTML += `<li>${key.dobbelsteenWaarde} = ${key.count} times</li>`;
+  document.querySelector(".countedValues").innerHTML = "";
+  history.countedLogs.forEach((key) => {
+    document.querySelector(
+      ".countedValues"
+    ).innerHTML += `<li>${key.dobbelsteenWaarde} = ${key.count} times</li>`;
   });
 }
